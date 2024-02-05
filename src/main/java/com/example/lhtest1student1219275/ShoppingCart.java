2d73d64a75b69918b48f7823c135218eb3cb0bf3
AkiFubuki7673 <tong.anton0625@gmail.com>
2024-02-05 14:10:56 -0500

    Question 3.2 complete

--- src/main/java/com/example/lhtest1student1219275/ShoppingCart.java
+++ src/main/java/com/example/lhtest1student1219275/ShoppingCart.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 public class ShoppingCart {
     private ArrayList<Component> arr1;
 
-
+    private void addItem(Component component){
+        arr1.add(component);
+    }
 
 }
