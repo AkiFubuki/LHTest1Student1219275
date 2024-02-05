71c74caed134dbbfa09ba99b68dcd6db6661f391
AkiFubuki7673 <tong.anton0625@gmail.com>
2024-02-05 13:14:03 -0500

    Initial commit

---
+++ src/main/java/com/example/lhtest1student1219275/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.lhtest1student1219275;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.lhtest1student1219275 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.lhtest1student1219275 to javafx.fxml;
+    exports com.example.lhtest1student1219275;
+}
