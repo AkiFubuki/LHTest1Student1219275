8321d2e43b8eecebfc42afda3146ebc14262fa96
AkiFubuki7673 <tong.anton0625@gmail.com>
2024-02-05 14:39:26 -0500

    Question 3.2 complete

--- src/main/java/com/example/lhtest1student1219275/ShoppingCart.java
+++ src/main/java/com/example/lhtest1student1219275/ShoppingCart.java
@@ -3,10 +3,25 @@ package com.example.lhtest1student1219275;
 import java.util.ArrayList;
 
 public class ShoppingCart {
-    private ArrayList<Component> arr1;
+    private ArrayList<Component> cart;
+    private double sum;
 
     private void addItem(Component component){
-        arr1.add(component);
+        cart= new ArrayList<>();
+    }
+
+    private double getTotalPrice(double price){
+        sum=price+price;
+
+        return sum;
+    }
+
+    public String getComponentNames(String ComponentNames){
+        return ComponentNames;
+    }
+    public String toString(){
+        int num;
+        return "The cart has "+num+" components with a total price of $"+sum;
     }
 
 }
